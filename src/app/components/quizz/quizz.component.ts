a872adbd8a0c2beee92d92f2202e47224e7ff7d8
♡Bia♡ <112487610+BiaAS2@users.noreply.github.com>
2024-02-17 14:40:44 -0300

    Project created

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css'],
+})
+export class QuizzComponent implements OnInit {
+  title: string = '';
+
+  questions: any;
+  quesntionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.startQuiz();
+  }
+
+  startQuiz(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.quesntionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  buttonPress(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.quesntionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+      // verificar resultado
+      console.log(this.answers);
+    }
+  }
+
+  async checkResult(anwers: string[]) {
+    const result = anwers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+    return result;
+  }
+
+  restartQuiz(): void {
+    this.answers = [];
+    this.questionIndex = 0;
+    this.finished = false;
+    this.startQuiz();
+  }
+}
